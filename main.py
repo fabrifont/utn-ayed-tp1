3d1b623fb68157bf67ba6c0214dd540d219c0345
Fabrizio Fontanarrosa <fabriziofontanarrosa@gmail.com>
2026-05-08 18:22:01 -0300

    Esqueleto del programa general creado

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-#
+# AyED 2026 - TP 1 - ISI 111
+# Integrantes:
+# - Fabrizio Fontanarrosa
+# - Clemente Giorgi
+# - Santiago Marchionatti
+# - Lautaro Casagrande
+
+# Definición de funciones
+
+def cartelInicio():
+    return
+
+def menu():
+    return
+
+def juego1():
+    return
+
+def juego2():
+    return
+
+def juego3():
+    return
+
+def juego4():
+    return
+
+def reporte():
+    return
+
+def salir():
+    return
+
+# Ejecución del programa
