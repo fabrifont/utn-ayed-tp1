d55199d8f4d0c449d95d79948bb9cd7d7922afa9
Fabrizio Fontanarrosa <fabriziofontanarrosa@gmail.com>
2026-05-08 19:51:26 -0300

    Menú principal creado

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+# TODO: Revisar lógica general del sistema, hacer algún diagrama de flujo para ver cómo 
+# acomodamos la ejecución de las funciones (while's, condicionales). Recordar que no se
+# permite el uso de break ni return vacío dentro de bucles ni exit
+
 # AyED 2026 - TP 1 - ISI 111
 # Integrantes:
 # - Fabrizio Fontanarrosa
@@ -17,9 +22,10 @@ def limpiarPantalla():
 
 def cartelInicio():
     limpiarPantalla()
+    print("")
     print("----------------------------------------------------------------------------------------------------------")
     print("")
-    print("Los juegos de apuesta están" + RED + " prohibidos " + RESET + "para los menores de 18 años, y son" + RED + " perjudiciales " + RESET + "para la salud.")
+    print("Los juegos de apuestas están" + RED + " prohibidos " + RESET + "para los menores de 18 años, y son" + RED + " perjudiciales " + RESET + "para la salud.")
     print("")
     print("Juegue por diversión. Juegue con responsabilidad.")
     print("")
@@ -31,6 +37,21 @@ def cartelInicio():
 def menu():
     limpiarPantalla()
     print("")
+    print("")
+    print(BLUE + "PYTHON " + BRIGHT_YELLOW + "CASINO" + RESET + " - " + GREEN + "MENÚ PRINCIPAL" + RESET)
+    print("")
+    print("Lista de opciones:")
+    print(GREEN + "A" + RESET + " - Juego del menor-mayor")
+    print(GREEN + "B" + RESET + " - Número secreto")
+    print(GREEN + "C" + RESET + " - Blackjack")
+    print(GREEN + "D" + RESET + " - Par o impar")
+    print(MAGENTA + "E" + RESET + " - Reporte")
+    print(RED + "F" + RESET + " - Salir")
+    print("")
+    print("")
+
+def validarOpcion(opcionIngresada):
+    return opcionIngresada == "A" or opcionIngresada == "B" or opcionIngresada == "C" or opcionIngresada == "D" or opcionIngresada == "E" or opcionIngresada == "F" or opcionIngresada == "a" or opcionIngresada == "b" or opcionIngresada == "c" or opcionIngresada == "d" or opcionIngresada == "e" or opcionIngresada == "f"
 
 def juego1():
     return
@@ -39,7 +60,7 @@ def juego2():
     return
 
 def juego3():
-    return
+    print("Juego en construcción. Volvé pronto!")
 
 def juego4():
     return
@@ -48,16 +69,24 @@ def reporte():
     return
 
 def salir():
-    return
+    continuar = False
 
 """
 Declaración de variables
-sistemaOperativo, comandoLimpiar: string
+sistemaOperativo, comandoLimpiar, opcion: string
+continuar: bool
 """
 
 sistemaOperativo = os.name
 comandoLimpiar = "cls" if sistemaOperativo == "nt" else "clear"
-
+continuar = True
+opcion = ""
+opcionEsValida = False
 # Ejecución del programa
 
 cartelInicio()
+menu()
+opcion = input("Elija una opción: ")
+""" while continuar:
+    while not opcionEsValida:
+        opcionEsValida = validarOpcion(opcion) """
