585109eac4193113da435950dff4a1e0e0b5ebb9
Fabrizio Fontanarrosa <fabriziofontanarrosa@gmail.com>
2026-05-29 20:50:27 -0300

    copiar constantes de colores al archivo main

--- main.py
+++ main.py
@@ -6,10 +6,26 @@
 # - Lautaro Casagrande
 
 # Bibliotecas
-
-from colores import *
 import os
 import random
+
+# Constantes de colores
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"  # orange on some systems
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+
+BRIGHT_RED = "\033[91m"
+BRIGHT_GREEN = "\033[92m"
+BRIGHT_YELLOW = "\033[93m"
+BRIGHT_BLUE = "\033[94m"
+BRIGHT_MAGENTA = "\033[95m"
+BRIGHT_CYAN = "\033[96m"
+WHITE = "\033[97m"
+
+RESET = "\033[0m"
 
 # Definición de funciones
 
